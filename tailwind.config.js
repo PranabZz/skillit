ee4eca8fea7f23cd9d9c642473bdcf05840c4d5b
Pranab Kc <pranabkca321@gmail.com>
2022-07-03 20:07:39 +0545

    index page added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    height: {
+      '280': '280px'
+    },
+    width: {
+      '700' : '700px',
+      '350' : '350px'
+    },
+  },
+  plugins: [],
+}
